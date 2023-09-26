01717025616e0b542c146b478d8f1f51add3facd
Shaila <shailaperezfernandez@gmail.com>
2023-09-26 16:47:55 +0200

    Estructura proyecto
    Estructura proyecto

--- src/main/java/com/shaila/ecommerce/Application.java
+++ src/main/java/com/shaila/ecommerce/Application.java
@@ -1,4 +1,4 @@
-package com.hiberus.arquetipo.crud;
+package com.shaila.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
